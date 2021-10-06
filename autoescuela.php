ccc46609d1e9fa87dc5801e73f5cb5b831fa38e1
CristianJRM <cristianjrm97@gmail.com>
2021-10-06 11:19:40 +0100

    Estilizando
    Mejorando indentación

--- autoescuela.php
+++ autoescuela.php
@@ -1,70 +1,78 @@
 <?php
 
-    $preguntas = [
-        ["num" => "1",
-        "pregunta" =>" Circulando por una vía frecuentada por peatones, especialmente niños o ancianos, ¿qué haremos?",
-        "opciones" => 
-            ["A" => " Reducir la velocidad, incluso llegando a detenerme.", 
+$preguntas = [
+    [
+        "num" => "1",
+        "pregunta" => " Circulando por una vía frecuentada por peatones, especialmente niños o ancianos, ¿qué haremos?",
+        "opciones" =>
+        [
+            "A" => " Reducir la velocidad, incluso llegando a detenerme.",
             "B" => " Adoptaré las medidas necesarias para su seguridad, sin tener que moderar obligatoriamente la velocidad.",
-            ], 
-        "respuesta" => 0,   
-          "imagen" => "",
-          "pista" => ""
         ],
+        "respuesta" => 0,
+        "imagen" => "images/1.jpeg"
+    ],
 
-            ["num" => "2",
-            "pregunta" =>"Circulando por una carretera de dos sentidos y tres carriles, dos para mi sentido y uno para el contrario, ¿para qué se utilizará el carril central?",
-            "opciones" => 
-                ["A" => "El central para los vehículos más rápidos y el de la derecha para los vehículos lentos.", 
-                "B" => "Para adelantar.", 
-                "C" => "Para adelantar o cuando las circunstancias del tráfico o de la vía lo aconsejen."],
-            "respuesta" => 0,
-              "imagen" => "",
-              "pista" => ""
-            ],
+    [
+        "num" => "2",
+        "pregunta" => "Circulando por una carretera de dos sentidos y tres carriles, dos para mi sentido y uno para el contrario, ¿para qué se utilizará el carril central?",
+        "opciones" =>
+        [
+            "A" => "El central para los vehículos más rápidos y el de la derecha para los vehículos lentos.",
+            "B" => "Para adelantar.",
+            "C" => "Para adelantar o cuando las circunstancias del tráfico o de la vía lo aconsejen."
+        ],
+        "respuesta" => 0,
+        "imagen" => "images/2.jpeg"
+    ],
 
-                ["num" => "3",
-                "pregunta" =>"¿El pasajero de una moto debe llevar abrochado el casco?",
-                "opciones" => 
-                    ["A" => "Sí, y además el conductor es responsable de que lo lleve abrochado.", 
-                    "B" => "Sí, siendo el pasajero el único responsable.", 
-                    "C" => "No es obligatorio, pero si aconsejable."],
-                "respuesta" => 0,   
-                  "imagen" => "",
-                  "pista" => ""
-                ],
+    [
+        "num" => "3",
+        "pregunta" => "¿El pasajero de una moto debe llevar abrochado el casco?",
+        "opciones" =>
+        [
+            "A" => "Sí, y además el conductor es responsable de que lo lleve abrochado.",
+            "B" => "Sí, siendo el pasajero el único responsable.",
+            "C" => "No es obligatorio, pero si aconsejable."
+        ],
+        "respuesta" => 0,
+        "imagen" => "images/3.jpeg"
+    ],
 
-                    ["num" => "4",
-                    "pregunta" =>"¿Qué significa una luz amarilla fija?",
-                    "opciones" => 
-                        ["A" => "Extremar la precaución.", 
-                        "B" => "Igual que una luz roja, detenerse en condiciones de seguridad.", 
-                        "C" => "Ceder el paso."],
-                    "respuesta" => 0,   
-                      "imagen" => "",
-                      "pista" => ""
-                    ],
+    [
+        "num" => "4",
+        "pregunta" => "¿Qué significa una luz amarilla fija?",
+        "opciones" =>
+        [
+            "A" => "Extremar la precaución.",
+            "B" => "Igual que una luz roja, detenerse en condiciones de seguridad.",
+            "C" => "Ceder el paso."
+        ],
+        "respuesta" => 0,
+        "imagen" => "images/4.jpeg"
+    ],
 
-                        ["num" => "5",
-                        "pregunta" =>"Si un agente está moviendo su brazo de arriba abajo, ¿qué está indicando?",
-                        "opciones" => 
-                            ["A" => "Que nos detengamos.", 
-                            "B" => "Que disminuyamos la velocidad.", 
-                            "C" => "Que cambiemos de dirección hacia el lado que tiene el brazo."],
-                        "respuesta" => 0,   
-                          "imagen" => "",
-                          "pista" => ""
-                        ],
-                    ];
+    [
+        "num" => "5",
+        "pregunta" => "Si un agente está moviendo su brazo de arriba abajo, ¿qué está indicando?",
+        "opciones" =>
+        [
+            "A" => "Que nos detengamos.",
+            "B" => "Que disminuyamos la velocidad.",
+            "C" => "Que cambiemos de dirección hacia el lado que tiene el brazo."
+        ],
+        "respuesta" => 0,
+        "imagen" => "images/5.jpeg"
+    ],
+];
 
-    function inicializarTest($preguntas){
-        foreach($preguntas as $pregunta){
-            echo '<div class="pregunta" '. '"pregunta-'.$pregunta['num']. '"'.">";
-            echo "<p><span>".$pregunta['num']."</span>"." ".$pregunta['pregunta']. "</p>";
-            foreach($pregunta['opciones'] as $letra => $enunciado){
-                echo $letra . " ) " . $enunciado . "</br>";
-            }
-            echo "</div>";
+function inicializarTest($preguntas){
+    foreach ($preguntas as $pregunta) {
+        echo '<div class="pregunta" ' . '"pregunta-' . $pregunta['num'] . '"' . ">";
+        echo "<p><span>" . $pregunta['num'] . "</span>" . " " . $pregunta['pregunta'] . "</p>";
+        foreach ($pregunta['opciones'] as $letra => $enunciado) {
+            echo $letra . " ) " . $enunciado . "</br>";
         }
+        echo "</div>";
     }
-?>
+}
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,18 +8,24 @@
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <?php
-        include "autoescuela.php";
+    include "autoescuela.php";
 
     ?>
-
+    <!-- TODO
+    * Mostrar respuesta, siendo en verde si acertó o en rojo si no acertó.
+    * Sumar la puntuación de las respuestas positivas.
+    * Mejorar su aspecto visual con CSS
+-->
     <div class="container">
         <form action="<?= $_SERVER['PHP_SELF'] ?>">
-        
-            <?php inicializarTest($preguntas);?>
+
+            <?php inicializarTest($preguntas); ?>
 
         </form>
     </div>
 </body>
+
 </html>
